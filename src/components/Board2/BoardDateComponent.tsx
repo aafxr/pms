c82e2e7039e0a2c153cf0959955e5f3b2f1032b7
aafxr <alexandral.fed23@gmail.com>
2024-06-21 17:30:51 +0700

    21/06/2024

--- src/components/Board2/BoardDateComponent.tsx
+++ src/components/Board2/BoardDateComponent.tsx
@@ -2,27 +2,20 @@ import React, {useEffect, useRef} from 'react';
 import {DateRange} from "../../core/classes/v1/DateRange";
 
 
-export interface BoardDateComponentPropsType{
+export interface BoardDateComponentPropsType {
     rang: DateRange
 }
 
 export function BoardDateComponent({rang}: BoardDateComponentPropsType) {
-    const dateRef = useRef<HTMLDivElement>(null)
-
-
-    useEffect(() => {
-        const el = dateRef.current
-        if(!el) return
-        el.querySelectorAll<HTMLDivElement>('.month').forEach(el => {
-            el.style.gridColumn = `span ${el.dataset.span}`
-        })
-    }, []);
-
-
     return (
-        <div ref={dateRef} className="date syncWheel">
+        <div className="date syncWheel">
             {Object.entries(rang.getMonths).map(([month, span], i) => (
-                <div key={i} className="month boarder" title={month} data-span={span}>
+                <div
+                    key={i}
+                    className="month boarder"
+                    title={month}
+                    style={{gridColumn: `span ${span}`}}
+                    data-span={span}>
                     <span>{month}</span>
                 </div>
             ))}
